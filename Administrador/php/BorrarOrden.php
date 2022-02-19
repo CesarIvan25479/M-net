d70181f0c6616e6ae547155f887191e2f0b17da1
Cesar Ivan <cesarivan25479@gmail.com>
2022-02-19 13:15:48 -0600

    Archivo para borrar orden borrar imagen de la carpeta

--- Administrador/php/BorrarOrden.php
+++ Administrador/php/BorrarOrden.php
@@ -1,9 +1,17 @@
 <?php
 include "Conexion.php";
 $folio = $_POST['folio'];
+$Destino = $_SERVER['DOCUMENT_ROOT'] . '/M-net/Imagenes/';
 $data = array();
+
+$consulta2 = "SELECT ImgOrden, ImgCredencial FROM ordenes WHERE folio='$folio'";
+$resultado = mysqli_query($Conexion,$consulta2);
+$datoimg = mysqli_fetch_array($resultado);
+
 $consulta = "DELETE FROM ordenes WHERE Folio='$folio'";
 if(mysqli_query($Conexion,$consulta)){
+    unlink($Destino.$datoimg['ImgOrden']);
+    unlink($Destino.$datoimg['ImgCredencial']);
     $data["estado"] = "si";
     echo json_encode($data);
 }else{

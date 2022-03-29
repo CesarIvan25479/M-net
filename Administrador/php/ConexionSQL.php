01abf197fb38bf76c95c7ce543e5b8d95f87bc8b
Cesar Ivan <cesarivan25479@gmail.com>
2022-03-29 11:33:27 -0600

    Conexion a base de datos

--- Administrador/php/ConexionSQL.php
+++ Administrador/php/ConexionSQL.php
@@ -1,13 +1,13 @@
 <?php
-$servidor = '192.168.3.102\SQLEXPRESS,1400';
+/*$servidor = '192.168.3.102\SQLEXPRESS,1400';
 $infoConex = array("Database" => "C:\MyBusinessDatabase\MyBusinessPOS2011.mdf","UID" => "CesarIvan", "PWD" => "123456789","CharacterSet" => "UTF-8");
 $Conn=sqlsrv_connect($servidor, $infoConex);
 if( $Conn === false ) {
     die( print_r( sqlsrv_errors(), true));
-}
-/*$servidor = '192.168.3.21\SQLEXPRESS,1400';
+}*/
+$servidor = '192.168.3.21\SQLEXPRESS,1400';
 $infoConex = array("Database" => "C:\MyBusinessDatabase\MyBusinessPOS2011_cli.mdf","UID" => "Inicio001", "PWD" => "123456789","CharacterSet" => "UTF-8");
-$Conn=sqlsrv_connect($servidor, $infoConex);*/
+$Conn=sqlsrv_connect($servidor, $infoConex);
 
 /* = '25.4.83.205\SQLEXPRESS,1433';
 $infoConex = array("Database" => "C:\MyBusinessDatabase\MyBusinessPOS2011_cli.mdf","UID" => "Inicio2021", "PWD" => "CEsarivan123","CharacterSet" => "UTF-8");
